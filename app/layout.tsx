f5b27c91fbaab80d18660de5a10dfd63ea5ba8e9
ala <alamissaoui.dev@gmail.com>
2025-10-08 12:10:22 +0100

    fix speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body className={inter.className}>
-        {/* <CustomCursor /> */}
+        <CustomCursor />
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
